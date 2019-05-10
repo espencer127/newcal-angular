7f67515205f6ddbadb18f9e89739d51746fd45b0
espencer127 <evanispencer@gmail.com>
2019-05-09 20:10:45 -0400

    trying to fix xml error

--- server.js
+++ server.js
@@ -21,17 +21,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'dist')));
 
 // Set our api routes
-app.use('/api', api);
-
-
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  
-  next();
-});
-
+app.use('/api/', api);
 
 
 // Catch all other routes and return the index file
--- src/services/event.service.ts
+++ src/services/event.service.ts
@@ -13,8 +13,8 @@ export class EventService {
   constructor(private http: HttpClient) {   }
 
   BASEURL = 'http://newcal-angular.herokuapp.com';
-  PORT = 'process.env.PORT || 3000';
-  api_url = `${this.BASEURL}:${this.PORT}`;
+  PORT = process.env.PORT || 3000;
+  api_url = `http://localhost:${this.PORT}`;
   newcalUrl = `${this.api_url}/api`;
 
   //Get the list of events
